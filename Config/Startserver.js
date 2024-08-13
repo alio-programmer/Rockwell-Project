9c750467b05835a83cba0fe091f86dc4dd0510f3
Gaurav singh bisht <gauravsinghbisht3@gmail.com>
2024-08-13 23:09:25 +0530

    fix

--- Config/Startserver.js
+++ Config/Startserver.js
@@ -17,11 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://rockwell-frontend.onrender.com",
-    ],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
