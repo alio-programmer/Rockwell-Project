96a6e76f3916e2537a2dfb18e229202203e69d34
Gaurav singh bisht <gauravsinghbisht3@gmail.com>
2024-08-14 22:38:50 +0530

    fix

--- Common/Generatetoken.js
+++ Common/Generatetoken.js
@@ -5,9 +5,12 @@ const generatetoken = (id, res) => {
   });
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: "strict",
+    // secure: process.env.NODE_ENV === "production" ? true : false,
+    // strict: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     domain: process.env.NODE_ENV === "production" ? ".onrender.com" : "",
+    path: "/",
   });
 };
 
