b214f8812a7f60ae5928d618d1ce0f371d0f8935
Gaurav Singh Bisht <92017948+alio-programmer@users.noreply.github.com>
2025-03-21 23:06:49 +0530

    Update Startserver.js

--- Config/Startserver.js
+++ Config/Startserver.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
